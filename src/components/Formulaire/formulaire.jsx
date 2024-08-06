1e0e2bedc0bf70f37217df3adf1b38083dd2fc36
Vigup02 <166111910+Vigup02@users.noreply.github.com>
2024-08-07 00:12:37 +0200

    formulaire commencé

---
+++ src/components/Formulaire/formulaire.jsx
@@ -0,0 +1,101 @@
+// Composant du formulaire
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import s from './formulaire.module.sass';
+import SecondaryButton from '../Buttons/SecondaryButton/SecondaryButton'; // Assurez-vous que le chemin est correct
+
+const SignupForm = () => {
+  const { register, handleSubmit, formState: { errors }, reset } = useForm();
+  const [confirmationMessage, setConfirmationMessage] = useState('');
+  const [showModal, setShowModal] = useState(false);
+
+  const onSubmit = data => {
+    console.log(data);
+
+    // Simuler l'envoi de l'email (remplacez par la logique réelle)
+    // e.g., axios.post('/send-email', data);
+
+    setConfirmationMessage('Votre message a été envoyé avec succès !');
+    setShowModal(true);
+    reset();
+
+    // Optionnel : Réinitialiser le message après 5 secondes
+    setTimeout(() => {
+      setConfirmationMessage('');
+      setShowModal(false);
+    }, 5000);
+  };
+
+  return (
+    <div className={s.formCont}>
+      <h2>Envie d'en savoir plus ou de partager vos idées ?
+      Laissez-moi votre message ici</h2>
+      <form onSubmit={handleSubmit(onSubmit)} className={s.form}>
+        <div className={s.nameContainer}>
+          <div className={s.formGroup}>
+            <input
+              {...register('lastName', { required: 'Un nom est requis' })}
+              className={s.inputField}
+              placeholder="Nom"
+            />
+            {errors.lastName && <span className={s.errorMessage}>{errors.lastName.message}</span>}
+          </div>
+          <div className={s.formGroup}>
+            <input
+              {...register('firstName', { required: 'Un prénom est requis' })}
+              className={s.inputField}
+              placeholder="Prénom"
+            />
+            {errors.firstName && <span className={s.errorMessage}>{errors.firstName.message}</span>}
+          </div>
+        </div>
+        <div className={s.formGroup}>
+          <input
+            {...register('phoneNumber', { required: 'Un numéro de téléphone est requis' })}
+            className={s.inputField}
+            placeholder="Numéro de téléphone"
+          />
+          {errors.phoneNumber && <span className={s.errorMessage}>{errors.phoneNumber.message}</span>}
+        </div>
+        <div className={s.formGroup}>
+          <input
+            type="email"
+            {...register('email', { required: 'Un email est requis' })}
+            className={s.inputField}
+            placeholder="Email"
+          />
+          {errors.email && <span className={s.errorMessage}>{errors.email.message}</span>}
+        </div>
+        <div className={s.formGroup}>
+          <textarea
+            {...register('about', { required: 'Précisez l\'objet' })}
+            className={s.inputField}
+            placeholder="À propos de quoi ?"
+          />
+          {errors.about && <span className={s.errorMessage}>{errors.about.message}</span>}
+        </div>
+        <div className={s.formGroup}>
+          <textarea
+            {...register('message', { required: 'Votre message est requis' })}
+            className={s.textArea}
+            placeholder="Votre message"
+          />
+          {errors.message && <span className={s.errorMessage}>{errors.message.message}</span>}
+        </div>
+        </form>
+        <SecondaryButton className={s.button} text="Envoyer" onClick={handleSubmit(onSubmit)} />
+        
+      {/* Affichage du message de confirmation */}
+      {showModal && (
+        <div className={s.modal}>
+          <div className={s.modalContent}>
+            <span className={s.close} onClick={() => setShowModal(false)}>&times;</span>
+            <p>{confirmationMessage}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SignupForm;
--- src/pages/Salon/Salon.jsx
+++ src/pages/Salon/Salon.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
-import s from './style.module.css';
+import SignupForm from '../../components/Formulaire/formulaire';
+import s from './salon.module.sass';
 
 const Salon = () => {
+  
+  return (
+    <div className={s.salon}>
+      <div className={s.salon_hero}>
 
-return (
-    <div className={s['Salon_Hero']}>
-      
+      </div>
+      <div className={s.salon_maude}>
+        
+      </div>
+      <div className={s.salon_valeur}>
+        
+      </div>
+      <div className={s.salon_salon}>
+        
+      </div>
+      <div className={s.salon_contact}>
+        
+      </div>
+      <div className={s.salon_formulaire}>
+        <SignupForm/>
+      </div>
     </div>
   );
 };
+
 export default Salon;
