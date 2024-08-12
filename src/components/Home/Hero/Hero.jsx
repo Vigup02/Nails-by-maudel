42d22a95fadf9eee474930c8fb19e62646a8a988
Vigup02 <166111910+Vigup02@users.noreply.github.com>
2024-08-12 17:16:37 +0200

    hero home fini et ajusté padding header

--- src/components/Home/Hero/Hero.jsx
+++ src/components/Home/Hero/Hero.jsx
@@ -12,19 +12,19 @@ const Hero = () => {
 return (
     <div className={s.homeHero}>
       <div className={s.homeHero_txt}>
-        <h1>L'Art des Ongles, </h1>
-        <h1>une Evasion Créative</h1>
-        <p>Plongez dans un voyage artistique avec des designs de nail art uniques, raffinés et empreints de caractère. 
-        Chaque création raconte une histoire, la vôtre.</p>
+        <h1>
+          <span>L'Art des Ongles,</span>
+          <span>une Evasion Créative</span>
+        </h1>
+        <h3>
+          <span>Plongez dans un voyage artistique avec des designs de nail art uniques et raffinés.</span> 
+          <span>Chaque création raconte une histoire, la vôtre.</span>
+        </h3>
         <PrimaryButton text="Trouvez votre style" onClick={() => navigate('/galerie')} />
       </div>
-      <div className={s.homeHero_img}>
-        <div>
-          <img className={s.homeHero_img_fleur} src={fleurHero} alt="Fleur" />
-        </div>
-        <div>
-          <img className={s.homeHero_img_main} src={mainHero} alt="Main_hero" />
-        </div>
+      <div className={s.homeHero_img}>     
+        <img className={s.homeHero_img_fleur}src={fleurHero} alt="Fleur" />
+        <img className={s.homeHero_img_main} src={mainHero} alt="Main_hero" />
       </div>
     </div>
   );
