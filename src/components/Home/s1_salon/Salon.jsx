349f2f80e3f58733735aa03f13b688c7fe728ac4
Vigup02 <166111910+Vigup02@users.noreply.github.com>
2024-08-12 20:32:38 +0200

    s1 salon desktop

--- src/components/Home/s1_salon/Salon.jsx
+++ src/components/Home/s1_salon/Salon.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
+import vitrineSalon from '../../../assets/Image/home/Vitrine.PNG';
+import brancheFeuille from '../../../assets/icône_illustration/fleur/branche_6_feuille_removebg.png';
+import SecondaryButton from '../../Buttons/SecondaryButton/SecondaryButton';
 import s from './salon.module.sass';
 
 const Salon = () => {
+    const navigate = useNavigate();
 
 return (
-    <div className={s.s1salon}>
-      
+    <div className={s.s1Salon}>
+      <div className={s.s1Salon_bkgrd}>
+      </div>
+      <div className={s.s1Salon_container}>
+        <img className={s.s1Salon_container_imgvitrine}src={vitrineSalon} alt="Vitrine" />
+        <div className={s.s1Salon_container_txt}>
+          <h2>LE SALON</h2>
+          <h3>Un Salon où chaleur et élégance se rencontrent</h3>
+          <p>Entrez dans un espace empreint de chaleur et de caractère, où l'authenticité se marie avec une élégance moderne. Chaque coin de notre salon reflète une histoire unique, créant une atmosphère accueillante et inspirante.</p>
+          <SecondaryButton text="Découvrez l’univers du salon" onClick={() => navigate('/salon')} />
+        </div>
+        <img className={s.s1Salon_container_icone}src={brancheFeuille} alt="branches6feuilles" />
+      </div>
     </div>
   );
 };
