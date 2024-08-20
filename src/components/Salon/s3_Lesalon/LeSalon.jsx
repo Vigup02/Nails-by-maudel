66cc5e995b524261dbe478125462eac0699cde2e
Vigup02 <166111910+Vigup02@users.noreply.github.com>
2024-08-20 10:59:40 +0200

    Page Salon parte Le Salon faite

---
+++ src/components/Salon/s3_Lesalon/LeSalon.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Comptoir from '../../../assets/Image/Salon/salon10.png';
+import Salon1 from '../../../assets/Image/Salon/salon2.webp';
+import Salon2 from '../../../assets/Image/Salon/salon3.webp';
+import Salon3 from '../../../assets/Image/Salon/salon6.webp';
+import Salon4 from '../../../assets/Image/Salon/salon8.webp';
+import s from './LeSalon.module.sass';
+
+const S3Salon = () => {
+
+
+return (
+  <section className={s.leSalon}>
+    <h2 className={s.leSalon_title}>LE SALON</h2>
+    <h3>Le salon est un havre de chaleur et de raffinement.</h3>
+    <h3>Chaque élément du décor est soigneusement sélectionné pour créer une ambiance accueillante et élégante, mêlant des touches vintage à des accents modernes.</h3>
+    <div className={s.leSalon_galerie}>
+      <img className={s.leSalon_galerie_img1} src={Comptoir} alt="comptoir du salon"/>
+      <div className={s.leSalon_galerie_cont}>
+        <div className={s.leSalon_galerie_cont_item}>
+          <img className={s.leSalon_galerie_cont_item_img} src={Salon1} alt="salle d'attente"/>
+          <img className={s.leSalon_galerie_cont_item_img} src={Salon2} alt="salle de travail"/>
+        </div>
+        <div className={s.leSalon_galerie_cont_item}>
+          <img className={s.leSalon_galerie_cont_item_img} src={Salon3} alt="vitrine de nuit"/>
+          <img className={s.leSalon_galerie_cont_item_img} src={Salon4} alt="comptoir du salon"/>
+        </div>
+      </div>
+    </div>
+  </section>
+  );
+};
+export default S3Salon;
--- src/pages/Salon/Salon.jsx
+++ src/pages/Salon/Salon.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import HeroSalon from '../../components/Salon/Hero/HeroSalon';
 import S1Maude from '../../components/Salon/s1_Maude/s1Maude';
 import S2Valeur from '../../components/Salon/s2_Valeur/s2Valeur';
+import S3Salon from '../../components/Salon/s3_Lesalon/LeSalon';
 import SignupForm from '../../components/Formulaire/formulaire';
 import s from './salon.module.sass';
 
@@ -12,6 +13,7 @@ const Salon = () => {
       <HeroSalon/>
       <S1Maude/>
       <S2Valeur/>
+      <S3Salon/>
       <div id="formulaire-section" className={s.salon_formulaire}>
         <SignupForm/>
       </div>
