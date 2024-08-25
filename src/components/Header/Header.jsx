7c8cb7d5fc9a585c6bbb8651fa7c7b6e067b539b
Vigup02 <166111910+Vigup02@users.noreply.github.com>
2024-08-25 18:25:43 +0200

    menu burger : à 100%width au 1er plan et se ferme quand on clique sur lemenu déroulant

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,65 +1,73 @@
-// Importation des modules nécessaires
-import React, { useState } from 'react'; // Importation de la bibliothèque React pour créer des composants React.
+import React, { useState } from 'react'; // Importation de la bibliothèque React et du hook useState pour la gestion de l'état.
 import { Link, useLocation, useNavigate } from 'react-router-dom'; // Importation des hooks et composants de react-router-dom pour la navigation.
-import s from './header.module.sass'; // Importation du fichier de styles SASS spécifique au composant Header. `s` est l'alias pour accéder aux classes définies dans le fichier SASS.
-import logoImg from '../../assets/Logo/Logo_terra_cotta-removebg_header.webp'; // Importation de l'image du logo pour l'afficher dans l'en-tête.
-import NavBarButton from '../Buttons/NavBarButton/NavBarButton'; // Importation du composant NavBarButton utilisé pour les boutons de la barre de navigation.
-import ContactButton from '../Buttons/ContactButton/ContactButton'; // Importation du composant ContactButton utilisé pour le bouton de contact.
-import BurgerIcon from '../BurgerIcon/BurgerIcon'; // Importez le nouveau composant
+import s from './header.module.sass'; 
+import logoImg from '../../assets/Logo/Logo_terra_cotta-removebg_header.webp'; 
+import NavBarButton from '../Buttons/NavBarButton/NavBarButton'; 
+import ContactButton from '../Buttons/ContactButton/ContactButton'; 
+import BurgerIcon from '../BurgerIcon/BurgerIcon';
 
 const Header = () => {
-  // Déclaration du composant fonctionnel Header
-
-  // Hooks de navigation
   const navigate = useNavigate(); // Hook permettant de naviguer programatiquement vers une autre route.
   const location = useLocation(); // Hook permettant d'obtenir l'objet de localisation actuel pour vérifier l'URL.
-  const [menuOpen, setMenuOpen] = useState(false); // État pour suivre l'ouverture du menu
+  const [menuOpen, setMenuOpen] = useState(false); // État pour suivre l'ouverture du menu burger.
 
+  // Fonction pour basculer l'état du menu burger
   const toggleMenu = () => {
-    setMenuOpen(!menuOpen); // Inverse l'état de `menuOpen`
+    setMenuOpen(!menuOpen); // Inverse l'état de `menuOpen`.
   };
 
+  // Fonction pour gérer le clic sur un bouton de la barre de navigation
+  const handleNavBarClick = (path) => {
+    setMenuOpen(false); // Ferme le menu après la navigation.
+    navigate(path); // Navigation vers le chemin spécifié.
+  };
+
+  // Fonction pour gérer le clic sur le bouton de contact
   const handleContactClick = () => {
+    setMenuOpen(false); // Ferme le menu après la navigation.
     if (location.pathname === '/salon') {
+      // Si l'utilisateur est déjà sur la page Salon, faire défiler vers la section contact
       document.getElementById('contact-section').scrollIntoView({ behavior: 'smooth' });
     } else {
+      // Sinon, naviguer vers la page Salon avec un état pour faire défiler vers la section contact
       navigate('/salon', { state: { scrollToContact: true } });
     }
-  };// Navigation vers la page Salon et l'ancre
+  };
 
   return (
     <header className={s.headerContainer}>
-      {/* Conteneur principal de l'en-tête */}
-      
       <Link to="/" className={s.headerContainer_logo}>
         {/* Lien vers la page d'accueil */}
-        <img src={logoImg} alt="Logo" /> {/* Affichage du logo avec une image importée */}
+        <img src={logoImg} alt="Logo" />
       </Link>
-      
       <nav className={`${s.headerContainer_navbar} ${menuOpen ? s.open : ''}`}>
-        {/* Classe conditionnelle ajoutée à la barre de navigation en fonction de l'état de `menuOpen` */}
-        
+        {/* Barre de navigation */}
         <NavBarButton
           text="Le Salon"
-          onClick={() => navigate('/salon')} // Fonction appelée lors du clic pour naviguer vers la page `/salon`
+          onClick={() => handleNavBarClick('/salon')} // Utilisation de la fonction pour fermer le menu et naviguer
           isActive={location.pathname === '/salon'} // Détermine si le bouton doit être actif en fonction du chemin actuel
         />
         <NavBarButton
           text="Galerie"
-          onClick={() => navigate('/galerie')} // Fonction appelée lors du clic pour naviguer vers la page `/galerie`
+          onClick={() => handleNavBarClick('/galerie')} // Utilisation de la fonction pour fermer le menu et naviguer
           isActive={location.pathname === '/galerie'} // Détermine si le bouton doit être actif en fonction du chemin actuel
         />
         <NavBarButton
           text="Services & Tarifs"
-          onClick={() => navigate('/service')} // Fonction appelée lors du clic pour naviguer vers la page `/service`
+          onClick={() => handleNavBarClick('/service')} // Utilisation de la fonction pour fermer le menu et naviguer
           isActive={location.pathname === '/service'} // Détermine si le bouton doit être actif en fonction du chemin actuel
         />
         <ContactButton
           text="Contact"
-          onClick={handleContactClick} // Utilisation de la fonction pour gérer le clic
+          onClick={handleContactClick} // Utilisation de la fonction pour gérer le clic et fermer le menu
         />
       </nav>
-      <BurgerIcon className={s.headerContainer_burgerIcon} isOpen={menuOpen} toggleMenu={toggleMenu} /> {/* Utilisez le nouveau composant */}
+
+      <BurgerIcon
+        className={s.headerContainer_burgerIcon}
+        isOpen={menuOpen}
+        toggleMenu={toggleMenu} // Utilisation de la fonction pour basculer l'état du menu burger
+      />
     </header>
   );
 };
